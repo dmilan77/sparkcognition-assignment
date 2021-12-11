521eadf89cf28197341e06353de3b3b02ce15916
MilanDas <milan.das77@gmail.com>
2021-12-11 16:52:21 -0500

    adding .. Sat Dec 11 16:52:21 EST 2021

--- provider.tf
+++ provider.tf
@@ -6,13 +6,13 @@ terraform {
     }
   }
 }
-terraform {
-  backend "s3" {
-    bucket = "tf-state-dmilan"
-    key    = "sparkrecgnition"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "tf-state-dmilan"
+#     key    = "sparkrecgnition"
+#     region = "us-east-1"
+#   }
+# }
 
 provider "aws" {
   region = "us-east-1"
